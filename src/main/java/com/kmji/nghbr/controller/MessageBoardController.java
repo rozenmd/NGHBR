27d7e50eef9e718b92b6371ceebf85eaaece94dc
Jason Silver <silver.jase@gmail.com>
2015-09-17 18:28:05 +1000

    message board css

--- src/main/java/com/kmji/nghbr/controller/MessageBoardController.java
+++ src/main/java/com/kmji/nghbr/controller/MessageBoardController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MessageBoardController extends AbstractController {
-		@RequestMapping(value = { "/messageboard" }, method = RequestMethod.GET)
-		public String homePage(ModelMap model) {
-			model.addAttribute("greeting", "Hi, Welcome to mysite");
-			return "user/messageboard";
-		}
-
+	@RequestMapping(value = { "/messageboard" }, method = RequestMethod.GET)
+	public String homePage(ModelMap model) {
+		model.addAttribute("greeting", "Hi, Welcome to mysite");
+		return "user/messageboard";
 	}
+
+}
