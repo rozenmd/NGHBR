7849b1bdefc9661ee54ff363e139731881bab401
Jason Silver <silver.jase@gmail.com>
2015-09-17 16:59:48 +1000

    Setup message board controller and views.

---
+++ src/main/java/com/kmji/nghbr/controller/MessageBoardController.java
@@ -0,0 +1,16 @@
+package com.kmji.nghbr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MessageBoardController extends AbstractController {
+		@RequestMapping(value = { "/messageboard" }, method = RequestMethod.GET)
+		public String homePage(ModelMap model) {
+			model.addAttribute("greeting", "Hi, Welcome to mysite");
+			return "user/messageboard";
+		}
+
+	}
