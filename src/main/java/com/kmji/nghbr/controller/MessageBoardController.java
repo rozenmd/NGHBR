ef773d184b9e3d0686283eaac4745c2f1a1068ca
Jason Silver <silver.jase@gmail.com>
2015-09-18 19:29:46 +1000

    clean up controller

--- src/main/java/com/kmji/nghbr/controller/MessageBoardController.java
+++ src/main/java/com/kmji/nghbr/controller/MessageBoardController.java
@@ -24,12 +24,6 @@ public class MessageBoardController extends AbstractController {
 
 	@RequestMapping(value = { "/messageboard" }, method = RequestMethod.GET)
 	public String messageBoardPage(ModelMap model) {
-		Message message = new Message();
-		message.setText("this is a post");
-		message.setPostCode(2036);
-		message.setUsername("James");
-		messageService.save(message);
-		model.addAttribute("message", message);
 		return "user/messageboard";
 	}
 
